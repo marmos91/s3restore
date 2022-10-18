564e3cc787935847186ad3c8d54c73f91b4941b2
Marco Moschettini <marco.moschettini@cubbit.io>
2022-10-18 22:48:06 +0200

    chore: repo init

---
+++ restore.sh
@@ -0,0 +1,47 @@
+#!bin/bash
+set -e
+
+usage()
+{
+    echo "Usage: restore.sh <bucket>"
+}
+
+bucket=$1
+endpoint="https://s3.cubbit.io"
+profile="cubbit"
+extension="enc"
+
+if [[ "$bucket" == "" ]]; then
+    usage
+    echo "You need to specify a bucket" >&2
+    exit 1
+fi
+
+keystodelete=$(aws s3api list-object-versions --endpoint $endpoint --profile $profile --bucket $bucket --query "Versions[?ends_with(Key, '$extension') && IsLatest]" --output json)
+deletemarkers=$(aws s3api list-object-versions --endpoint $endpoint --profile $profile --bucket $bucket --query "DeleteMarkers" --output json)
+
+if [[ $(echo $keystodelete | jq -c .)  != "[]" && $(echo $keystodelete | jq -c .)  != "null" ]]; then
+    echo "Deleting keys..."
+    echo $keystodelete | jq -r '.[] | [.Key,.VersionId] | @csv' | while IFS="," read -r key version; do
+        key=$(echo $key | xargs)
+        version=$(echo $version | xargs)
+        echo "Deleting key=$key, version=$version"
+        aws s3api delete-object --endpoint "$endpoint" --profile "$profile" --bucket "$bucket" --key "$key" --version-id "$version" 2>&1 > /dev/null
+    done;
+else
+    echo "No keys to delete"
+fi
+
+if [[ $(echo $keystodelete | jq -c .)  != "[]" && $(echo $deletemarkers | jq -c .)  != "null" ]]; then
+    echo "Deleting delete markers..."
+    echo $deletemarkers | jq -r '.[] | [.Key,.VersionId] | @csv' | while IFS="," read -r key version; do
+        key=$(echo $key | xargs)
+        version=$(echo $version | xargs)
+        echo "Deleting key=$key, version=$version"
+        aws s3api delete-object --endpoint "$endpoint" --profile "$profile" --bucket "$bucket" --key "$key" --version-id "$version" 2>&1 > /dev/null
+    done;
+else
+    echo "No delete markers to delete"
+fi
+
+echo "Bucket $bucket restored"
---
+++ sync.sh
@@ -0,0 +1,40 @@
+#!bin/bash
+set -e
+
+local=$1
+remote=$2
+direction=$3
+
+usage()
+{
+    echo "Usage: sync.sh <local> <remote> <direction>"
+}
+
+if [[ "$local" == "" ]]; then
+    usage
+    echo "You need to specify a local path" >&2
+    exit 1
+fi
+
+if [[ "$remote" == "" ]]; then
+    usage
+    echo "You need to specify a remote path" >&2
+    exit 1
+fi
+
+case $direction in
+    up)
+        echo "Running rclone sync -P $local $remote"
+        rclone sync -P $local $remote
+    ;;
+    down)
+        echo "Running rclone sync -P $remote $local"
+        rclone sync -P $remote $local
+    ;;
+    *)
+        usage
+        echo "You have to specify a direction (up or down)" >&2
+        exit 1
+    ;;
+esac
+
